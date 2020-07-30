5feb883e26fdfd821b845edb72684da7999e2819
zkj <438101845@qq.com>
2020-07-30 23:40:19 +0800

    [zkj]-feat: finish exercise_02

--- src/exercise_02/characterCounter.js
+++ src/exercise_02/characterCounter.js
@@ -6,5 +6,17 @@ export default function countCharacter(string, prediction) {
   // * Please implement this function and pass all the tests in character_counter_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if (string === null || string === undefined || string === '') {
+    return 0;
+  }
+  if (prediction === undefined) {
+    return string.length;
+  }
+  let count = 0;
+  for (let i = 0; i < string.length; i += 1) {
+    if (prediction(string.charAt(i))) {
+      count += 1;
+    }
+  }
+  return count;
 }
