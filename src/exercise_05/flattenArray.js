913f3fb1633632d2a4dc4af9be033aaa871f0f43
zkj <438101845@qq.com>
2020-07-31 00:58:06 +0800

    [zkj]-feat: finish exercise_05

--- src/exercise_05/flattenArray.js
+++ src/exercise_05/flattenArray.js
@@ -6,5 +6,18 @@ export default function flattenArray(array) {
   // * Please implement this function and pass all the tests in flatten_array_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if (array === null || array === undefined) {
+    throw new Error('Flatten undefined or null array');
+  }
+  const res = [];
+  for (let i = 0; i < array.length; i += 1) {
+    if (Array.isArray(array[i])) {
+      for (let j = 0; j < array[i].length; j += 1) {
+        res.push(array[i][j]);
+      }
+    } else {
+      res.push(array[i]);
+    }
+  }
+  return res;
 }
