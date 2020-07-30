e628d851a856f40f6335a2922646bdb4b0c3ab7e
zkj <438101845@qq.com>
2020-07-30 23:12:23 +0800

    [zkj]-feat: finish exercise_01

--- src/exercise_01/numberFormatter.js
+++ src/exercise_01/numberFormatter.js
@@ -7,6 +7,8 @@ export default function formatNumber(number, option) {
   //
   // * Please implement the function and pass all the tests in format_number_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  if (option === undefined || option.currency === false) {
+    return `${number.toFixed(2)}`;
+  }
+  return `$ ${number.toFixed(2)}`;
 }
