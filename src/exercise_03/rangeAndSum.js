ec778893e81ea4afd70f946f7e8f5f6e4bef90c3
zkj <438101845@qq.com>
2020-07-30 23:59:22 +0800

    [zkj]-feat: finish exercise_03

--- src/exercise_03/rangeAndSum.js
+++ src/exercise_03/rangeAndSum.js
@@ -6,8 +6,18 @@ export function range(start, end) {
   //
   // * Please implement this function and pass all the tests in range_and_array_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  const res = [];
+  if (start < end) {
+    for (let i = start; i < end; i += 1) {
+      res.push(i);
+    }
+  }
+  if (start > end) {
+    for (let i = start; i > end; i -= 1) {
+      res.push(i);
+    }
+  }
+  return res;
 }
 
 export function sum(...numbers) {
@@ -17,6 +27,9 @@ export function sum(...numbers) {
   //
   // * Please implement this function and pass all the tests in range_and_array_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  let getSum = 0;
+  for (let i = 0; i < numbers.length; i += 1) {
+    getSum += numbers[i];
+  }
+  return getSum;
 }
