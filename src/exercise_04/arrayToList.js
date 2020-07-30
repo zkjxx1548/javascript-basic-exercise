bea22a4a1593d8b8258541e4490f9a24af0f7031
zkj <438101845@qq.com>
2020-07-31 00:37:34 +0800

    [zkj]-feat: finish exercise_04

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -5,6 +5,15 @@ export default function arrayToList(array) {
   //
   // * Please implement this function and pass all the tests in array_to_list_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  let list = null;
+  if (array === null || array === undefined) {
+    throw new Error('Creating list from undefined array');
+  }
+  if (array.length === 0) {
+    throw new Error('Creating list from empty array');
+  }
+  for (let i = array.length - 1; i >= 0; i -= 1) {
+    list = { value: array[i], next: list };
+  }
+  return list;
 }
